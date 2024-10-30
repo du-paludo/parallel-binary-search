487e3e192ff3d71fabda78d794ae9172b2c5ee68
Eduardo Paludo <esp21@inf.ufpr.br>
2024-10-30 19:09:32 -0300

    changes

--- main.c
+++ main.c
@@ -18,11 +18,14 @@ pthread_t threads[MAX_THREADS];
 int threads_ids[MAX_THREADS];
 pthread_barrier_t thread_barrier;
 
+ll* Pos;
+ll* Q;
+
 ll random_ll() {
     return (ll) rand() % LLONG_MAX;
 }
 
-int lower_bound(int arr[], int n, int x) {
+int lower_bound(int* arr, int n, int x) {
     int mid;
     int low = 0;
     int high = n;
@@ -49,7 +52,7 @@ void initialize_threads(int nThreads) {
     threads_ids[0] = 0;
     for(int i = 1; i < nThreads; i++) {
         threads_ids[i] = i;
-        pthread_create(&threads[i], NULL, reducePartialSum, &threads_ids[i]);
+        pthread_create(&threads[i], NULL, , &threads_ids[i]);
     }
 }
 
@@ -57,62 +60,66 @@ int main(int argc, char* argv[]) {
     int i;
     int nThreads;
     int nTotalElements;
+    int nQueries;
     chronometer_t parallelBinarySearchTime;
 
     srand(time(NULL));
 
-    if (argc != 3) {
-         printf("Usage: %s <nTotalElements> <nThreads>\n", argv[0]); 
+    if (argc != 4) {
+         printf("Usage: %s <nTotalElements> <nThreads> <nQueries>\n", argv[0]); 
          return 0;
     } else {
         nThreads = atoi(argv[2]);
         if (nThreads == 0) {
-            printf("Usage: %s <nTotalElements> <nThreads>\n", argv[0]);
+            printf("Usage: %s <nTotalElements> <nThreads> <nQueries>\n", argv[0]);
             printf("<nThreads> can't be 0\n");
             return 0;
         }     
         if (nThreads > MAX_THREADS) {  
-            printf("Usage: %s <nTotalElements> <nThreads>\n", argv[0]);
+            printf("Usage: %s <nTotalElements> <nThreads> <nQueries>\n", argv[0]);
             printf( "<nThreads> must be less than %d\n", MAX_THREADS);
             return 0;
         }
         nTotalElements = atoi(argv[1]);
         if (nTotalElements > MAX_TOTAL_ELEMENTS) {
-            printf("Usage: %s <nTotalElements> <nThreads>\n", argv[0]);
+            printf("Usage: %s <nTotalElements> <nThreads <nQueries>\n", argv[0]);
             printf( "<nTotalElements> must be up to %d\n", MAX_TOTAL_ELEMENTS);
             return 0;
         }
+        nQueries = atoi(argv[3]);
     }
-    printf("Will use %d threads to run binary search on %d total long long elements\n\n", nThreads, nTotalElements);
+    printf("Will use %d threads to run binary search on %d total long long elements %d times\n\n", nThreads, nTotalElements, nQueries);
 
 
     // Inicializações
-    ll* inputVector = malloc(sizeof(ll) * nTotalElements);
-
+    Pos = malloc(sizeof(ll) * nTotalElements);
     for (int i = 0; i < nTotalElements; i++) {
-        inputVector[i] = random_ll();
+        Pos[i] = random_ll();
+    }
+
+    Q = malloc(sizeof(ll) * nQueries);
+    for (int i = 0; i < nQueries; i++) {
+        Q[i] = random_ll();
     }
 
     chrono_reset(&parallelBinarySearchTime);
     chrono_start(&parallelBinarySearchTime);
-    int start_position = 0;
 
     // Execução
-    ll* inVec = &inVec[start_position];
     initialize_threads(nThreads);
 
     chrono_stop(&parallelBinarySearchTime);
     chrono_reportTime(&parallelBinarySearchTime, "parallelBinarySearchTime");
 
+    // int arr[] = { 4, 6, 10, 12, 18, 20 };
+    // int n = sizeof(arr) / sizeof(arr[0]);
 
-    int arr[] = { 4, 6, 10, 12, 18, 20 };
-    int n = sizeof(arr) / sizeof(arr[0]);
+    // int x = 7;
 
-    int x = 7;
+    // printf("%d\n", lower_bound(arr, n, x));
 
-    printf("%d\n", lower_bound(arr, n, x));
-
-    free(inputVector);
+    free(Pos);
+    free(Q);
 
     return 0;
 }
