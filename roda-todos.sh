b781ab335050922fa3771aa017ffe8c92b37eac5
Natael Pontarolo Gomes <nataelpontarolo@gmail.com>
2024-11-04 17:32:09 -0300

    script

--- roda-todos.sh
+++ roda-todos.sh
@@ -13,12 +13,28 @@ for i in {1..8}
 do
     echo "Executando 10 vezes com $nElements elementos e $i threads:"
     total_time=0    
+    total_throughput=0
+
     for j in {1..10}
     do
-        # Execute o programa e filtre o tempo
-        tempo=$(./teste $nElements $i $nQueries | grep -oP '(?<=tempo total: )[^ ]*')
-        echo "Execução $j: $tempo segundos"
+        output=$(./teste $nElements $i $nQueries)
 
-        #total_time=$(echo "$total_time + $tempo" | bc)
+        # Execute o programa e filtre o tempo
+        tempo=$(echo "$output" | grep -oP '(?<=tempo total: )[^ ]*')
+        throughput=$(echo "$output" | grep -oP '(?<=Throughput: )[^ ]*')
+
+        echo "Tempo: $tempo nanosegundos"
+        echo "Throughput: $throughput operações por segundo"
+
+        # Soma o tempo total e o throughput para cálculo da média
+        total_time=$(echo "$total_time + $tempo" | bc -l)
+        total_throughput=$(echo "$total_throughput + $throughput" | bc -l)
     done
+
+    # Calcula média de tempo e throughput
+    avg_time=$(echo "$total_time / 10" | bc -l)
+    avg_throughput=$(echo "$total_throughput / 10" | bc -l)
+
+    echo "Tempo médio: $avg_time segundos"
+    echo "Throughput médio: $avg_throughput operações por segundo"
 done
